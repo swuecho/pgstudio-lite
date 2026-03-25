9d1fa9e5d23d8bb99c5a4ee9b751df5ac1b0ba71
Hao Wu <echowuhao@gmail.com>
2026-03-25 10:43:09 +0800

    Simplify meta-db and add vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,6 @@ export default defineConfig({
   test: {
     include: ['tests/**/*.test.ts'],
     exclude: ['**/node_modules/**', '**/.git/**', '**/.test-dist/**'],
+    setupFiles: ['tests/setup.ts'],
   },
 })
